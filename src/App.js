7e0a965aa27409d1dc8c6b0861eaec86a9018ea6
Jerry Victor <jerryvictor@jerrys-mbp.myfiosgateway.com>
2023-03-28 14:04:24 -0400

    onDoubleClick toggle reminder

--- src/App.js
+++ src/App.js
@@ -30,10 +30,15 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className="container">
       <Header title= 'Task Tracker' />
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks')}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks')}
     </div>
   );
 }
