73457e7cd5f8b7065552da68449dd01023ca527b
Jerry Victor <jerryvictor@jerrys-mbp.myfiosgateway.com>
2023-03-28 12:38:16 -0400

    set state on App.js, instead of Tasks.js to use globally

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import Header from "./components/Header";
+import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import { useState } from "react"
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1, 
+        text: 'Doctors Appointment', 
+        day: 'April 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School', 
+        day: 'April 6th at 1:30pm', 
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Food Shopping', 
+        day: 'April 7th at 12:30pm', 
+        reminder: true
+    }
+])
+
   return (
     <div className="container">
       <Header title= 'Task Tracker' />
-      <Tasks /> 
+      <Tasks tasks={tasks} /> 
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,5 @@
-import { useState } from 'react'
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1, 
-            text: 'Doctors Appointment', 
-            day: 'April 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at School', 
-            day: 'April 6th at 1:30pm', 
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'Food Shopping', 
-            day: 'April 7th at 12:30pm', 
-            reminder: true
-        }
-    ])
-
+const Tasks = ({ tasks }) => {
     return (
         <div>
             {tasks.map((task) => (
